8e91e3c7477bd5523a050fc97fefe02249ccbb0c
Nelson Bassey <nellybaz10@gmail.com>
2021-05-28 09:52:55 +0200

    Add credit and debit methods of balanceService

--- balance-service/src/index.ts
+++ balance-service/src/index.ts
@@ -1,3 +1,39 @@
-class BalanceService{
-  
+export interface User{
+  id:string
+}
+
+export interface Repository{
+  has:(userId:string)=>Promise<boolean>,
+  get:(userId:string) => Promise<number>,
+  put:(userId:string, amount:number)=>Promise<boolean>
+}
+
+export class BalanceService{
+  repository;
+  constructor(repository:Repository){
+    this.repository = repository;
+  }
+
+  async balance(user:User):Promise<number>{
+    if(await !this.repository.has(user.id)) {
+      return 0;
+    }
+    return this.repository.get(user.id);
+  }
+
+  async credit(user:User, amount:number):Promise<number>{
+    let currentAmount = await this.repository.get(user.id);
+    let newAmount = currentAmount + amount;
+    let creditRes = await this.repository.put(user.id, newAmount);
+    if(creditRes) return newAmount;
+    throw Error('Error crediting balance')
+  }
+
+  async debit(user:User, amount:number):Promise<number>{
+    let currentAmount = await this.repository.get(user.id);
+    let newAmount = currentAmount - amount;
+    let creditRes = await this.repository.put(user.id, newAmount);
+    if(creditRes) return newAmount;
+    throw Error('Error debiting balance')
+  }
 }
--- balance-service/src/test/balance-service.ts
+++ balance-service/src/test/balance-service.ts
@@ -1,8 +1,103 @@
-import {expect} from 'chai';
+import { expect } from 'chai';
 import 'mocha'
+import { User, BalanceService, Repository } from '../index';
 
-describe('Balance service', ()=>{
-  it('returns correct user balance', ()=>{
-    expect(1).to.eq(1);
+describe('Balance service', () => {
+  let repo: Repository;
+  beforeEach(() => {
+    repo = { get: () => Promise.resolve(0), has: () => Promise.resolve(true), put: () => Promise.resolve(false) };
+  });
+  describe('Balance', () => {
+    it('returns 0 for user with no balance', async () => {
+      let user: User = {
+        id: '123'
+      }
+      let balanceService = new BalanceService(repo);
+
+      expect(await balanceService.balance(user)).to.eq(0);
+    })
+
+    it('returns zero if user ID is not in the balance DB', async () => {
+      let user: User = {
+        id: '123'
+      }
+      let balanceService = new BalanceService(repo);
+
+      expect(await balanceService.balance(user)).to.eq(0);
+    })
+
+    it('returns correct balance from DB', async () => {
+      let user: User = {
+        id: '123'
+      }
+      repo.get = () => Promise.resolve(100);
+      let balanceService = new BalanceService(repo);
+
+      expect(await balanceService.balance(user)).to.eq(100);
+    })
+  })
+
+  describe('Credit', () => {
+
+    it('adds to user balance correctly', async () => {
+      let user: User = {
+        id: '123'
+      }
+      repo.get = () => Promise.resolve(100);
+      repo.put = () => Promise.resolve(true);
+      let balanceService = new BalanceService(repo);
+
+      expect(await balanceService.balance(user)).to.eq(100);
+      expect(await balanceService.credit(user, 100)).to.eq(200);
+    })
+
+    it('throws error when cannot credit balance', async () => {
+      let user: User = {
+        id: '123'
+      }
+      repo.get = () => Promise.resolve(100);
+      repo.put = () => Promise.resolve(false);
+      let balanceService = new BalanceService(repo);
+
+      expect(await balanceService.balance(user)).to.eq(100);
+      try {
+        const response = await balanceService.credit(user, 100)
+        expect(response).to.eq(0);
+      } catch (error) {
+        expect(error.message).to.eq("Error crediting balance")
+      }
+    })
+  })
+
+  describe('Debit', () => {
+
+    it('subtracts from user balance correctly', async () => {
+      let user: User = {
+        id: '123'
+      }
+      repo.get = () => Promise.resolve(100);
+      repo.put = () => Promise.resolve(true);
+      let balanceService = new BalanceService(repo);
+
+      expect(await balanceService.balance(user)).to.eq(100);
+      expect(await balanceService.debit(user, 100)).to.eq(0);
+    })
+
+    it('throws error when cannot debit from balance', async () => {
+      let user: User = {
+        id: '123'
+      }
+      repo.get = () => Promise.resolve(100);
+      repo.put = () => Promise.resolve(false);
+      let balanceService = new BalanceService(repo);
+
+      expect(await balanceService.balance(user)).to.eq(100);
+      try {
+        const response = await balanceService.debit(user, 100)
+        expect(response).to.eq(200);
+      } catch (error) {
+        expect(error.message).to.eq("Error debiting balance")
+      }
+    })
   })
 })
